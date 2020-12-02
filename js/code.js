93436fb9fb007a7811a72cbf01d6245a90547315
qwertyzzzzz <57785323+qwertyzzzzz@users.noreply.github.com>
2020-12-02 21:33:49 +0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,5 +1,5 @@
 "use strict";
-let taskList = document.querySelector('ul');
+const taskList = document.querySelector('ul');
 loadData();
 hidespisok();
 //удаление
@@ -31,8 +31,7 @@ for (let redT of redTask){
 }
 //появление "дел нет"
 function hidespisok(){
-let spz=document.querySelector('ul');
-if(spz.children.length>1)
+if(taskList.children.length>1)
 {
     document.getElementById('empty-list-item').style.visibility = "hidden";
 }
